f499752a113b0e42c3e256b36be28052b58994ed
jd34226 <jd6233@srmist.edu.in>
2026-05-22 17:16:12 +0530

    Create MemberRepository.java
    Added Repo for Member

---
+++ Backend/Member/MemberRepository.java
@@ -0,0 +1,11 @@
+package mgt.library.member.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import mgt.library.member.model.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    // Find member by username (for login)
+    Optional<Member> findByUsername(String username);
+}
