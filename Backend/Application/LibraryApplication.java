1139bbb60897d7ca620717c075abadf9518954cd
jd34226 <jd6233@srmist.edu.in>
2026-05-22 17:13:58 +0530

    Create LibraryApplication.java
    Added the main file

---
+++ Backend/Application/LibraryApplication.java
@@ -0,0 +1,37 @@
+package mgt.library.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@ComponentScan(basePackages = { "mgt.library.transaction", "mgt.library.member", "mgt.library.book" })
+@EnableJpaRepositories(basePackages = {"mgt.library.transaction.repository", "mgt.library.member.repository","mgt.library.book.repository"})
+@EntityScan(basePackages = {"mgt.library.transaction.model", "mgt.library.member.model","mgt.library.book.model" })
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+	    return new WebMvcConfigurer() {
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/**")
+	                .allowedOrigins(
+	                    "http://libraryapplication.mgmt",
+	                    "http://libraryapplication.mgmt:3000"
+	                )
+	                .allowedMethods("GET", "POST", "PUT", "DELETE")
+	                .allowCredentials(true);
+	        }
+	    };
+	}
+}
