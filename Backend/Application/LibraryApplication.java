099f76924f3b1edbbf778b33b096288b75d89c35
NixieCalin <yc7243@srmist.edu.in>
2026-06-15 21:38:05 +0530

    Update LibraryApplication.java

--- Backend/Application/LibraryApplication.java
+++ Backend/Application/LibraryApplication.java
@@ -1,5 +1,4 @@
 package mgt.library.application;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
